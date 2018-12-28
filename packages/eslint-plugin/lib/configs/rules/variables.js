b7bb616028018a6b465090dfba377ab9712928a5
Petter Walbø Johnsgård <petter@dekode.no>
2018-12-28 23:20:33 +0100

    Add no-unused-vars

--- packages/eslint-plugin/lib/configs/rules/variables.js
+++ packages/eslint-plugin/lib/configs/rules/variables.js
@@ -8,4 +8,12 @@ module.exports = {
 	// Disallow the use of undeclared variables unless mentioned in /*global */ comments
 	// https://eslint.org/docs/rules/no-undef
 	'no-undef': 'error',
+
+	// Disallow unused variables
+	// https://eslint.org/docs/rules/no-unused-vars
+	'no-unused-vars': [ 'error', {
+		vars: 'all',
+		args: 'after-used',
+		ignoreRestSiblings: true,
+	} ],
 };

a2a2a948ffd96830e003e78808a8340b17bdfcf2
Petter Walbø Johnsgård <petter@dekode.no>
2018-12-28 23:06:48 +0100

    Add no-undef

--- packages/eslint-plugin/lib/configs/rules/variables.js
+++ packages/eslint-plugin/lib/configs/rules/variables.js
@@ -4,4 +4,8 @@ module.exports = {
 	// Disallow deleting variables
 	// https://eslint.org/docs/rules/no-delete-var
 	'no-delete-var': 'error',
+
+	// Disallow the use of undeclared variables unless mentioned in /*global */ comments
+	// https://eslint.org/docs/rules/no-undef
+	'no-undef': 'error',
 };

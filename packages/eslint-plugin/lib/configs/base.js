7a36b7d337b8baf24d51c59bcfaefe43e43acc68
Petter Walbø Johnsgård <petter@dekode.no>
2018-12-30 16:07:45 +0100

    Add config options

--- packages/eslint-plugin/lib/configs/base.js
+++ packages/eslint-plugin/lib/configs/base.js
@@ -4,6 +4,17 @@
 const merge = require( 'merge' );
 
 module.exports = {
+	parser: 'babel-eslint',
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
 	plugins: [ 'import' ],
 	rules: merge(
 		require( './rules/best-practices' ),
